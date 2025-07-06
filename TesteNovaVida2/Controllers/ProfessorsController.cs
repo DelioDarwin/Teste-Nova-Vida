4fcba31a4453b9caee5a3d4629461aceb3e5a356
Delio Darwin <delio.darwin@itt.org.br>
2025-07-06 02:16:29 -0300

    Primeiro Commit
    Primeiro Commit

---
+++ TesteNovaVida2/Controllers/ProfessorsController.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Humanizer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
+using TesteNovaVida2.Data;
+using TesteNovaVida2.Models;
+
+
+namespace TesteNovaVida2.Controllers
+{
+    public class ProfessorsController : Controller
+    {
+        private readonly TesteNovaVida2Context _context;
+
+        public ProfessorsController(TesteNovaVida2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Professors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Professor.ToListAsync());
+        }
+
+        // GET: Professors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professor
+                .FirstOrDefaultAsync(m => m.IdProfessor == id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // GET: Professors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Professors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProfessor,NomeProfessor")] Professor professor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(professor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(professor);
+        }
+
+        // GET: Professors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professor.FindAsync(id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+            return View(professor);
+        }
+
+        // POST: Professors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdProfessor,NomeProfessor")] Professor professor)
+        {
+            if (id != professor.IdProfessor)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(professor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProfessorExists(professor.IdProfessor))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(professor);
+        }
+
+        // GET: Professors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var professor = await _context.Professor
+                .FirstOrDefaultAsync(m => m.IdProfessor == id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return View(professor);
+        }
+
+        // POST: Professors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var professor = await _context.Professor.FindAsync(id);
+            if (professor != null)
+            {
+                _context.Professor.Remove(professor);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProfessorExists(int id)
+        {
+            return _context.Professor.Any(e => e.IdProfessor == id);
+        }
+
+
+        public async Task<IActionResult> ListarAlunosProfessor(int? id, string? nome)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            IEnumerable<object> nomeRet = null;
+
+            List<Aluno> aluno =  _context.Aluno
+                .Include(a => a.Professor)
+               .Where(m => m.IdProfessor == id)
+                .ToListAsync().Result;
+
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+         
+
+            return View(aluno);
+        }
+
+
+        [HttpPost, ActionName("DeleteDirect")]
+        public async Task DeleteDirect(int id)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var alunos = _context.Aluno.Where(c => c.IdProfessor == id).ToList();
+                foreach (var x in alunos)
+                {
+                    var com = x;
+                    _context.Aluno.Remove(com);
+                }
+
+                await _context.SaveChangesAsync();
+
+                var profesor = await _context.Professor.FindAsync(id);
+                _context.Professor.Remove(profesor);
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                BadRequest(ModelState);
+            }
+            
+        }
+    }
+}
---
+++ TesteNovaVida2/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using System.Web.Mvc;
+using TesteNovaVida2;
+using TesteNovaVida2.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TesteNovaVida2Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("TesteNovaVida2Context") ?? throw new InvalidOperationException("Connection string 'TesteNovaVida2Context' not found.")));
+
+
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+
+    var cultureInfo = new CultureInfo("pt-BR");
+    cultureInfo.NumberFormat.CurrencySymbol = "R$";
+
+    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+    CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+    var supportedCultures = new[] { "pt-BR" }; // Brazilian Portuguese
+    options.DefaultRequestCulture = new RequestCulture("pt-BR");
+
+    options.AddInitialRequestCultureProvider(new CustomRequestCultureProvider(async context =>
+    {
+    // My custom request culture logic
+    return await Task.FromResult(new ProviderCultureResult("pt-BR"));
+    }));
+});
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+   app.UseRequestLocalization(); // Add this line
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "Professor",
+    pattern: "{controller=Professors}/{action=Index}/{id?}").WithStaticAssets();
+
+app.Run();
